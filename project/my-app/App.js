241708c8f1dddee86627fca70cae88ac5a10e63a
jaggerchen2001 <jaggerchen2001@gmail.com>
2024-08-11 06:15:51 +0800

    Updated Article.js, ArticleWebView.js, App.js (add ArticleWebView Screen), Updated dependencies(webview version)

--- project/my-app/App.js
+++ project/my-app/App.js
@@ -1,29 +1,38 @@
-
 //App.js
 //import libraries
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 
 //import external module for navigation
-import HomeScreen from './Home';
-import FeedbackScreen from './Feedback';
-import ArticlesScreen from './Articles';
-import Social from './Social';
-import FoodScreen from './Food';
-import LoginScreen from './Login';
+import HomeScreen from "./Home";
+import FeedbackScreen from "./Feedback";
+import ArticlesScreen from "./Articles";
+import Social from "./Social";
+import FoodScreen from "./Food";
+import LoginScreen from "./Login";
+import ArticleWebView from './ArticleWebView';
 
 const Stack = createStackNavigator();
 
 const App = () => {
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} initialParams={{ theme: 'light' }} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          initialParams={{ theme: "light" }}
+        />
         <Stack.Screen name="Feedback" component={FeedbackScreen} />
         <Stack.Screen name="Articles" component={ArticlesScreen} />
+        <Stack.Screen
+          name="WebView"
+          component={ArticleWebView}
+          options={{ headerTitle: "Web View" }}
+        />
         <Stack.Screen name="Social" component={Social} />
         <Stack.Screen name="Food" component={FoodScreen} />
       </Stack.Navigator>
--- project/my-app/Articles.js
+++ project/my-app/Articles.js
@@ -1,26 +1,65 @@
-
 //Article.js
 //import libraries
-import React from 'react';
-import { View, Text, TouchableOpacity, Image, ScrollView, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
 
 //Stylesheet
 const styles = StyleSheet.create({
-  footer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
+  container: {
+    flex: 1,
+    padding: 20,
+    paddingBottom: 100,
+    marginBottom: 80,
+  },
+  articleContainer: {
+    marginBottom: 30,
     padding: 10,
-    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  description: {
+    fontSize: 14,
+    color: "#666",
+    marginVertical: 5,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    borderRadius: 5,
+    marginVertical: 5,
+  },
+  linkText: {
+    color: "blue",
+    textDecorationLine: "underline",
+  },
+  footer: {
+    position: "absolute",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "#fff",
     borderTopWidth: 1,
-    borderColor: '#e0e0e0',
+    borderColor: "#e0e0e0",
     left: 0,
     right: 0,
     bottom: 0,
-    top: 675,
   },
   footerButton: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   footerButtonIcon: {
     width: 24,
@@ -33,39 +72,98 @@ const styles = StyleSheet.create({
 });
 
 const ArticlesScreen = ({ navigation }) => {
+  const [articles, setArticles] = useState([]);
+
+  const URL =
+    "https://api.thenewsapi.com/v1/news/all?api_token=pP8Y5NTCGT4yRW0p9ka3uUa12yvmA0H79U8aC7Iz&search=calories+countArti";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(URL);
+        const json = await response.json();
+        setArticles(json.data); // Assuming the data is in the 'data' field of the JSON
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <ScrollView style={styles.container}>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView style={styles.container}>
+        {articles.map((article) => (
+          <View key={article.uuid} style={styles.articleContainer}>
+            <Text style={styles.title}>{article.title}</Text>
+            <Text style={styles.description}>{article.description}</Text>
+            <Image source={{ uri: article.image_url }} style={styles.image} />
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("WebView", { url: article.url })
+              }
+            >
+              <Text style={styles.linkText}>Read More</Text>
+            </TouchableOpacity>
+          </View>
+        ))}
+      </ScrollView>
 
-      <Text>Article Screen</Text>
+      <Text>Article</Text>
       <View style={styles.footer}>
-        <TouchableOpacity style={styles.footerButton} onPress={() => navigation.navigate('Feedback')}>
-          <Image source={require('../../project/my-app/assets/Feedback.png')} style={styles.footerButtonIcon} />
+        <TouchableOpacity
+          style={styles.footerButton}
+          onPress={() => navigation.navigate("Feedback")}
+        >
+          <Image
+            source={require("../../project/my-app/assets/Feedback.png")}
+            style={styles.footerButtonIcon}
+          />
           <Text style={styles.footerButtonText}>Feedback</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity style={styles.footerButton} onPress={() => navigation.navigate('Articles')}>
-          <Image source={require('../../project/my-app/assets/Articles.png')} style={styles.footerButtonIcon} />
+        <TouchableOpacity
+          style={styles.footerButton}
+          onPress={() => navigation.navigate("Articles")}
+        >
+          <Image
+            source={require("../../project/my-app/assets/Articles.png")}
+            style={styles.footerButtonIcon}
+          />
           <Text style={styles.footerButtonText}>Article</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity style={styles.footerButton} onPress={() => navigation.navigate('Home')}>
-          <Image source={require('../../project/my-app/assets/Home.png')} style={styles.footerButtonIcon} />
+        <TouchableOpacity
+          style={styles.footerButton}
+          onPress={() => navigation.navigate("Home")}
+        >
+          <Image
+            source={require("../../project/my-app/assets/Home.png")}
+            style={styles.footerButtonIcon}
+          />
           <Text style={styles.footerButtonText}>Home</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity style={styles.footerButton} onPress={() => navigation.navigate('Social')}>
-          <Image source={require('../../project/my-app/assets/Social.png')} style={styles.footerButtonIcon} />
+        <TouchableOpacity
+          style={styles.footerButton}
+          onPress={() => navigation.navigate("Social")}
+        >
+          <Image
+            source={require("../../project/my-app/assets/Social.png")}
+            style={styles.footerButtonIcon}
+          />
           <Text style={styles.footerButtonText}>Social</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity style={styles.footerButton} onPress={() => navigation.navigate('Food')}>
-          <Image source={require('../../project/my-app/assets/Food.png')} style={styles.footerButtonIcon} />
+        <TouchableOpacity
+          style={styles.footerButton}
+          onPress={() => navigation.navigate("Food")}
+        >
+          <Image
+            source={require("../../project/my-app/assets/Food.png")}
+            style={styles.footerButtonIcon}
+          />
           <Text style={styles.footerButtonText}>Food</Text>
         </TouchableOpacity>
-
       </View>
-    </ScrollView>
+    </SafeAreaView>
   );
 };
 //export ArticleScreen as an external module for referencing
