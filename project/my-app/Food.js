49974a95ab5134319f8cc572fc9f00425392680f
chanyuxiang <chanyuxiangcyx@gmail.com>
2024-08-24 02:28:51 +0800

    Update on Food.js page with footer

--- project/my-app/Food.js
+++ project/my-app/Food.js
@@ -9,61 +9,90 @@ import {
   StyleSheet,
   FlatList,
   TouchableOpacity,
+  SafeAreaView,
 } from "react-native";
+import { useTheme } from "./ThemeContext";
+import Footer from './Footer.js'; // Adjust the path based on your project structure
 
-//Stylesheet
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
+// Define theme settings for light and dark modes
+const themes = {
+  light: {
     backgroundColor: "#fff",
-  },
-  label: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-  input: {
-    height: 40,
+    textColor: "#333",
     borderColor: "#ccc",
-    borderWidth: 1,
-    paddingHorizontal: 10,
-    marginBottom: 20,
+    linkColor: "blue",
   },
-  foodItem: {
-    padding: 10,
-    borderBottomColor: "#ccc",
-    borderBottomWidth: 1,
+  dark: {
+    backgroundColor: "#333",
+    textColor: "#fff",
+    borderColor: "#888",
+    linkColor: "lightblue",
   },
-  foodName: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  foodDetails: {
-    marginTop: 20,
-  },
-  macroContainer: {
-    marginTop: 20,
-  },
-  macro: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 10,
-  },
-  macroLabel: {
-    fontSize: 16,
-    flex: 1,
-  },
-  macroInput: {
-    flex: 2,
-    height: 40,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    paddingHorizontal: 10,
-  },
-});
+};
 
-const FoodScreen = () => {
+// Dynamic stylesheet function that adjusts styles based on the current theme
+const getStyles = (theme) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+      paddingBottom: 110, // Leave space for the footer
+      backgroundColor: themes[theme].backgroundColor,
+    },
+    label: {
+      fontSize: 18,
+      fontWeight: "bold",
+      marginBottom: 10,
+      color: themes[theme].textColor,
+    },
+    input: {
+      height: 40,
+      borderColor: themes[theme].borderColor,
+      borderWidth: 1,
+      paddingHorizontal: 10,
+      marginBottom: 20,
+      color: themes[theme].textColor,
+    },
+    foodItem: {
+      padding: 10,
+      borderBottomColor: themes[theme].borderColor,
+      borderBottomWidth: 1,
+    },
+    foodName: {
+      fontSize: 16,
+      fontWeight: "bold",
+      color: themes[theme].textColor,
+    },
+    foodDetails: {
+      marginTop: 20,
+    },
+    macroContainer: {
+      marginTop: 20,
+    },
+    macro: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginBottom: 10,
+    },
+    macroLabel: {
+      fontSize: 16,
+      flex: 1,
+      color: themes[theme].textColor,
+    },
+    macroInput: {
+      flex: 2,
+      height: 40,
+      borderColor: themes[theme].borderColor,
+      borderWidth: 1,
+      paddingHorizontal: 10,
+      color: themes[theme].textColor,
+    },
+  });
+
+const FoodScreen = ({ navigation }) => {
+  const { theme } = useTheme(); // Get the current theme
+  const styles = getStyles(theme); // Get styles based on the current theme
+
   const [query, setQuery] = useState("");
   const [foods, setFoods] = useState([]);
   const [selectedFood, setSelectedFood] = useState(null);
@@ -118,72 +147,75 @@ const FoodScreen = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Search for food</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Eg:100g of chicken thigh"
-        value={query}
-        onChangeText={setQuery}
-      />
-      <Button title="Search" onPress={handleSearch} />
-      {selectedFood ? (
-        <View style={styles.foodDetails}>
-          <Text style={styles.foodName}>{selectedFood.food_name}</Text>
-          <View style={styles.macroContainer}>
-            <View style={styles.macro}>
-              <Text style={styles.macroLabel}>Carbs</Text>
-              <TextInput
-                style={styles.macroInput}
-                value={carbs}
-                editable={false}
-              />
-            </View>
-            <View style={styles.macro}>
-              <Text style={styles.macroLabel}>Fats</Text>
-              <TextInput
-                style={styles.macroInput}
-                value={fats}
-                editable={false}
-              />
-            </View>
-            <View style={styles.macro}>
-              <Text style={styles.macroLabel}>Protein</Text>
-              <TextInput
-                style={styles.macroInput}
-                value={protein}
-                editable={false}
-              />
-            </View>
-            <View style={styles.macro}>
-              <Text style={styles.macroLabel}>Total calories</Text>
-              <TextInput
-                style={styles.macroInput}
-                value={calories}
-                editable={false}
-              />
-            </View>
-          </View>
-          <Button
-            title="Back to Search"
-            onPress={() => setSelectedFood(null)}
-          />
-        </View>
-      ) : (
-        <FlatList
-          data={foods}
-          keyExtractor={(item) => item.food_name}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              style={styles.foodItem}
-              onPress={() => selectFood(item)}
-            >
-              <Text style={styles.foodName}>{item.food_name}</Text>
-            </TouchableOpacity>
-          )}
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={styles.label}>Search for food</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Eg:100g of chicken thigh"
+          value={query}
+          onChangeText={setQuery}
         />
-      )}
-    </View>
+        <Button title="Search" onPress={handleSearch} />
+        {selectedFood ? (
+          <View style={styles.foodDetails}>
+            <Text style={styles.foodName}>{selectedFood.food_name}</Text>
+            <View style={styles.macroContainer}>
+              <View style={styles.macro}>
+                <Text style={styles.macroLabel}>Carbs</Text>
+                <TextInput
+                  style={styles.macroInput}
+                  value={carbs}
+                  editable={false}
+                />
+              </View>
+              <View style={styles.macro}>
+                <Text style={styles.macroLabel}>Fats</Text>
+                <TextInput
+                  style={styles.macroInput}
+                  value={fats}
+                  editable={false}
+                />
+              </View>
+              <View style={styles.macro}>
+                <Text style={styles.macroLabel}>Protein</Text>
+                <TextInput
+                  style={styles.macroInput}
+                  value={protein}
+                  editable={false}
+                />
+              </View>
+              <View style={styles.macro}>
+                <Text style={styles.macroLabel}>Total calories</Text>
+                <TextInput
+                  style={styles.macroInput}
+                  value={calories}
+                  editable={false}
+                />
+              </View>
+            </View>
+            <Button
+              title="Back to Search"
+              onPress={() => setSelectedFood(null)}
+            />
+          </View>
+        ) : (
+          <FlatList
+            data={foods}
+            keyExtractor={(item) => item.food_name}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={styles.foodItem}
+                onPress={() => selectFood(item)}
+              >
+                <Text style={styles.foodName}>{item.food_name}</Text>
+              </TouchableOpacity>
+            )}
+          />
+        )}
+      </View>
+      <Footer theme={theme} navigation={navigation} />
+    </SafeAreaView>
   );
 };
 
