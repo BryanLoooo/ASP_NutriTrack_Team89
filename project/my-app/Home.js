ef46d33281285faa9e28110406b202a2b621eab5
Bryan <Bryan@gmail.com>
2024-08-28 13:25:22 +0800

    update home page screen if the added comments

--- project/my-app/Home.js
+++ project/my-app/Home.js
@@ -1,5 +1,5 @@
 //Home.js
-//import libraries
+//import libraries and dependencies
 import React, { useState,  useEffect, useCallback } from "react";
 import { View, Text, TouchableOpacity, Image, ScrollView, StyleSheet, Alert } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -7,10 +7,10 @@ import { useFocusEffect } from "@react-navigation/native";
 import { useTheme } from "./ThemeContext";
 import Footer from "./Footer";
 
-//stylesheet
+//stylesheet for different user interface elements, it accomodates for both light and dark themes
 const styles = StyleSheet.create({
 
-  // Common styles
+  // Common styles for all themes
   container: {
     flex: 1,
     backgroundColor: "#f2f2f2",
@@ -88,7 +88,7 @@ const styles = StyleSheet.create({
     marginBottom: 100,
   },
 
-  // Light mode
+  // Light mode for specific styles
   light: {
     container: {
       flex: 1,
@@ -190,7 +190,7 @@ const styles = StyleSheet.create({
     },
   },
 
-  // Dark mode
+  // Dark mode for specfic styles
   dark: {
     container: {
       flex: 1,
@@ -294,238 +294,276 @@ const styles = StyleSheet.create({
   },
 });
 
+//Main homescreen2 component
 const HomeScreen2 = ({ navigation }) => {
-  const { theme, toggleTheme } = useTheme();
-  const [selectedArticle] = useState(null);
-  const [caloriesGoal] = useState(3000);
-  const [totalCarbs, setTotalCarbs] = useState(0);
-  const [totalFats, setTotalFats] = useState(0);
-  const [totalProtein, setTotalProtein] = useState(0);
-  const [totalCalories, setTotalCalories] = useState(0);
-  const maxCalories = 4000;
 
-  const fetchTotalNutrients = async () => {
-    try {
-      console.log("Retrieving total nutrients information");
-      const savedNutrients = await AsyncStorage.getItem("totalNutrients");
+  try{
 
-      if (savedNutrients) {
-        const parsedNutrients = JSON.parse(savedNutrients);
-        setTotalCarbs(parsedNutrients.carbs);
-        setTotalFats(parsedNutrients.fats);
-        setTotalProtein(parsedNutrients.protein);
+    console.log("Home screen page has been executed successfully");
 
-        if(totalCalories > 4000){
-          Alert.alert("Max calories for the day have been reached.");
-          console.log("Max limit for the day has been reached");
+    //get the current page theme and theme toggler from context
+    const { theme, toggleTheme } = useTheme();
 
-        }else if(totalCalories < 4000){
-          setTotalCalories(parsedNutrients.calories);
-          console.log("Limit for the day has not been reached. Calories added.");
+    //state the variables for tracking selected article
+    const [selectedArticle] = useState(null);
 
-        }else{
-          console.log("No food information has been logged");
-        }
-        
-        console.log("Successfully retrieved and update total nutrients information");
+    //state the variables for tracking selected nutrients information
+    const [caloriesGoal] = useState(3000);
+    const [totalCarbs, setTotalCarbs] = useState(0);
+    const [totalFats, setTotalFats] = useState(0);
+    const [totalProtein, setTotalProtein] = useState(0);
+    const [totalCalories, setTotalCalories] = useState(0);
 
-      } else {
-        console.log("Failed. No nutrients information found");
-      }
-    } catch (error) {
-      console.error("Failed. Error retrieving total nutrients information. Error description: ", error);
-    }
-  };
+    //set the maximum calories per day
+    const maxCalories = 4000;
 
-  useEffect(() => {
-    const resetDataOnStart = async () => {
+    //function to fetch stored nutrients information from AsyncStorage
+    const fetchTotalNutrients = async () => {
+
       try {
-        console.log("Reset nutrients information back to 0");
-        await AsyncStorage.removeItem("totalNutrients");
-        console.log("Successfully reset nutrients information");
+        console.log("Retrieving total nutrients information");
+        const savedNutrients = await AsyncStorage.getItem("totalNutrients");
 
+        //if nutrients information found display and update the information
+        if (savedNutrients) {
+          const parsedNutrients = JSON.parse(savedNutrients);
+          setTotalCarbs(parsedNutrients.carbs);
+          setTotalFats(parsedNutrients.fats);
+          setTotalProtein(parsedNutrients.protein);
+
+          //checks if the current totalCalories is within the range
+          if(totalCalories > 4000){
+            Alert.alert("Max calories for the day have been reached.");
+            console.log("Max limit for the day has been reached");
+
+          }else if(totalCalories < 4000){
+            setTotalCalories(parsedNutrients.calories);
+            console.log("Limit for the day has not been reached. Calories added.");
+
+          }else{
+            console.log("No food information has been logged");
+          }
+          
+          console.log("Successfully retrieved and update total nutrients information");
+
+          //returns a failed message if there is no nutrients information
+        } else {
+          console.log("Current nutrients information is all set to 0. Please add food items to see updated information");
+        }
       } catch (error) {
-        console.error("Failed. Error resetting nutrients information. Error description: ", error);
+        console.error("Failed. Error retrieving total nutrients information. Error description: ", error);
       }
     };
 
-    resetDataOnStart();
-    fetchTotalNutrients();
-  }, []
-);
+    //useEffect function used to set the nutrients information to 0
+    useEffect(() => {
+      const resetDataOnStart = async () => {
+        try {
 
-  useFocusEffect(
-    useCallback(() => {
-      try {
+          console.log("Reset nutrients information back to 0");
+          await AsyncStorage.removeItem("totalNutrients");
 
-        console.log("Fetching total nutrients information using focus");
-        fetchTotalNutrients();
-        console.log("Successfully fetched total nutrients information");
+          console.log("Successfully reset nutrients information");
 
-      } catch (error) {
-        console.error("Failed. Error during focus effect. Error description:", error);
-      }
-    }, [])
+          //returns a failed message if the nutrients information is not able to set to 0
+        } catch (error) {
+          console.error("Failed. Error resetting nutrients information. Error description: ", error);
+        }
+      };
+
+      //function call to reset the nutrients information set to 0
+      resetDataOnStart();
+
+      //function call to fetch nutrients information and display it
+      fetchTotalNutrients();
+    }, []
   );
 
-  // Check if calories exceed the maximum allowed
-  if (totalCalories > 3000) {
-    Alert.alert("Reminder, calories for the day have been reached. Don't give up!");
-    console.log("Calories set for the day has been reached.");
-  } else {
-    console.log("Calories for the day are still within range. User can continue consuming calories.");
-  }
+    //useFocusEffect function is used to fetch information when the screen comes into focus
+    useFocusEffect(
+      useCallback(() => {
+        try {
 
-  const articles = [
-    {
-      title: "How to eat healthy",
-      image: require("../my-app/assets/Article1.png"),
-    },
-    {
-      title: "Regular exercise helps",
-      image: require("../my-app/assets/Article2.png"),
-    },
-    {
-      title: "Balanced diet",
-      image: require("../my-app/assets/Article3.png"),
-    },
-    {
-      title: "Community and unity",
-      image: require("../my-app/assets/Article4.png"),
-    },
-    {
-      title: "Importance of health",
-      image: require("../my-app/assets/Article5.png"),
-    },
-  ];
+          console.log("Fetching total nutrients information using focus");
+          fetchTotalNutrients();
 
-  const calorieData = {
-    Monday: 1800,
-    Tuesday: 2500,
-    Wednesday: 3200,
-    Thursday: 2900,
-    Friday: 1500,
-    Saturday: 3700,
-    Sunday: 2200,
-  };
+          console.log("Successfully fetched total nutrients information");
 
-  // user interface elements
-  return (
-    <ScrollView style={styles[theme].container}>
-      <View style={styles[theme].header}>
-        <View style={styles.headerInner}>
-          <Image source={require("../my-app/assets/logoHome.png")} style={styles.titleImage} />
-          <TouchableOpacity
-            style={styles[theme].themeButton}
-            onPress={() => {
-              try {
-                console.log("Theme button has been triggered");
-                toggleTheme();
-              } catch (error) {
-                console.error("Failed. Error toggling theme. Error description:", error);
-              }
-            }}
-          >
-            <Image
-              source={
-                theme === "light"
-                  ? require("../../project/my-app/assets/Sun.png")
-                  : require("../../project/my-app/assets/Moon.png")
-              }
-              style={styles[theme].themeButtonImage}
-            />
-          </TouchableOpacity>
-        </View>
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={styles.articleList}
-        >
-          {articles.map((article) => (
+          //returns and error of the function did not work as expected
+        } catch (error) {
+          console.error("Failed. Error during focus effect. Error description:", error);
+        }
+      }, [])
+    );
+
+    // Check if calories exceed the maximum allowed
+    if (totalCalories > 3000) {
+
+      //returns an alert on the user interface when the user has consumed more than the daily set goal
+      Alert.alert("Reminder, calories for the day have been reached. Don't give up!");
+      console.log("Calories set for the day has been reached.");
+
+      //if the total daily set calories are within range, no alert is generated and the user can continue insertion
+    } else {
+      console.log("Calories for the day are still within range. User can continue consuming calories.");
+    }
+
+    //dataset for the list of articles for the user to explore
+    const articles = [
+      {
+        title: "How to eat healthy",
+        image: require("../my-app/assets/Article1.png"),
+      },
+      {
+        title: "Regular exercise helps",
+        image: require("../my-app/assets/Article2.png"),
+      },
+      {
+        title: "Balanced diet",
+        image: require("../my-app/assets/Article3.png"),
+      },
+      {
+        title: "Community and unity",
+        image: require("../my-app/assets/Article4.png"),
+      },
+      {
+        title: "Importance of health",
+        image: require("../my-app/assets/Article5.png"),
+      },
+    ];
+
+    //dataset for the list of caloriesData for the past week
+    const calorieData = {
+      Monday: 1800,
+      Tuesday: 2500,
+      Wednesday: 3200,
+      Thursday: 2900,
+      Friday: 1500,
+      Saturday: 3700,
+      Sunday: 2200,
+    };
+
+    // return the JSX elements that make up the user interface
+    return (
+      <ScrollView style={styles[theme].container}>
+        <View style={styles[theme].header}>
+          <View style={styles.headerInner}>
+            <Image source={require("../my-app/assets/logoHome.png")} style={styles.titleImage} />
             <TouchableOpacity
-              key={article.title}
-              style={[
-                styles.articleButton,
-                selectedArticle === article.title && styles.selectedArticleButton,
-              ]}
+              style={styles[theme].themeButton}
               onPress={() => {
                 try {
-                  console.log(`Navigating to ${article.title} article`);
-                  navigation.navigate("Articles");
-                  console.log("Successfully navigation to selected article")
+                  console.log("Theme button has been triggered");
+                  toggleTheme();
                 } catch (error) {
-                  console.error("Failed. Error navigating to articles. Error description: ", error);
+                  console.error("Failed. Error toggling theme. Error description:", error);
                 }
               }}
             >
-              <Image source={article.image} style={styles.articleImage} />
-              <Text style={styles.articleButtonText}>{article.title}</Text>
+              <Image
+                source={
+                  theme === "light"
+                    ? require("../../project/my-app/assets/Sun.png")
+                    : require("../../project/my-app/assets/Moon.png")
+                }
+                style={styles[theme].themeButtonImage}
+              />
             </TouchableOpacity>
-          ))}
-        </ScrollView>
-      </View>
-      <View style={styles[theme].postContainer}>
-        <TouchableOpacity 
-          style={styles[theme].postButton} 
-          onPress={() => {
-            try {
-              console.log("Navigating to social page");
-              navigation.navigate("Social");
-              console.log("Successful navigation to social page")
-            } catch (error) {
-              console.error("Failed. Error navigating to social page. Error description: ", error);
-            }
-          }}
-        >
-          <Text style={styles[theme].postButtonText}>+ Add post</Text>
-        </TouchableOpacity>
-      </View>
-
-      <Text style={styles[theme].dailyTitle}>Today's progress: </Text>
-      <View style={styles[theme].dailyProgressContainer}>
-        <View style={styles.dailyProgressBar}>
-          <View style={[styles.dailyProgressFill, { width: `${(totalCalories / maxCalories) * 100}%`, backgroundColor: totalCalories > 3000 ? "red" : "green", maxWidth: "100%" }]} />
-        </View>
-        <View style={styles[theme].limitContainer}>
-          <Text style={styles[theme].limitText}>
-              Daily limit: {caloriesGoal} cals
-          </Text>
-          <Text style={styles[theme].limitText}>
-              Max: {maxCalories} cals
-          </Text>
-      </View>
-        <View style={styles[theme].statsContainer}>
-          <Text style={styles[theme].dailyProgressText}>
-            Calories: {totalCalories} cals
-          </Text>
-          <Text style={styles[theme].dailyProgressText}>
-            Goal: {caloriesGoal} cals
-          </Text>
-          <Text style={styles[theme].dailyProgressText}>Protein: {totalProtein}g</Text>
-          <Text style={styles[theme].dailyProgressText}>Fats: {totalFats}g</Text>
-          <Text style={styles[theme].dailyProgressText}>
-            Carbohydrates: {totalCarbs}g
-          </Text>
-        </View>
-      </View>
-
-      <View style={styles.historyProgressContainer}>
-        {Object.entries(calorieData).map(([day, calories]) => (
-          <View style={styles[theme].historyProgressItem} key={day}>
-            <Text style={styles[theme].historyProgressText}>{day}</Text>
-            <View
-              style={[
-                styles.historyProgressFill,
-                { backgroundColor: calories > 3000 ? "red" : "green", width: `${(calories / 5000) * 100}%` },
-              ]}
-            />
           </View>
-        ))}
-      </View>
-      <Footer theme={theme} navigation={navigation} />
-    </ScrollView>
-  );
-};
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.articleList}
+          >
+            {articles.map((article) => (
+              <TouchableOpacity
+                key={article.title}
+                style={[
+                  styles.articleButton,
+                  selectedArticle === article.title && styles.selectedArticleButton,
+                ]}
+                onPress={() => {
+                  try {
+                    console.log(`Navigating to ${article.title} article`);
+                    navigation.navigate("Articles");
+                    console.log("Successfully navigation to selected article")
+                  } catch (error) {
+                    console.error("Failed. Error navigating to articles. Error description: ", error);
+                  }
+                }}
+              >
+                <Image source={article.image} style={styles.articleImage} />
+                <Text style={styles.articleButtonText}>{article.title}</Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+        <View style={styles[theme].postContainer}>
+          <TouchableOpacity 
+            style={styles[theme].postButton} 
+            onPress={() => {
+              try {
+                console.log("Navigating to social page");
+                navigation.navigate("Social");
+                console.log("Successful navigation to social page")
+              } catch (error) {
+                console.error("Failed. Error navigating to social page. Error description: ", error);
+              }
+            }}
+          >
+            <Text style={styles[theme].postButtonText}>+ Add post</Text>
+          </TouchableOpacity>
+        </View>
 
+        <Text style={styles[theme].dailyTitle}>Today's progress: </Text>
+        <View style={styles[theme].dailyProgressContainer}>
+          <View style={styles.dailyProgressBar}>
+            <View style={[styles.dailyProgressFill, { width: `${(totalCalories / maxCalories) * 100}%`, backgroundColor: totalCalories > 3000 ? "red" : "green", maxWidth: "100%" }]} />
+          </View>
+          <View style={styles[theme].limitContainer}>
+            <Text style={styles[theme].limitText}>
+                Daily limit: {caloriesGoal} cals
+            </Text>
+            <Text style={styles[theme].limitText}>
+                Max: {maxCalories} cals
+            </Text>
+        </View>
+          <View style={styles[theme].statsContainer}>
+            <Text style={styles[theme].dailyProgressText}>
+              Calories: {totalCalories} cals
+            </Text>
+            <Text style={styles[theme].dailyProgressText}>
+              Goal: {caloriesGoal} cals
+            </Text>
+            <Text style={styles[theme].dailyProgressText}>Protein: {totalProtein}g</Text>
+            <Text style={styles[theme].dailyProgressText}>Fats: {totalFats}g</Text>
+            <Text style={styles[theme].dailyProgressText}>
+              Carbohydrates: {totalCarbs}g
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.historyProgressContainer}>
+          {Object.entries(calorieData).map(([day, calories]) => (
+            <View style={styles[theme].historyProgressItem} key={day}>
+              <Text style={styles[theme].historyProgressText}>{day}</Text>
+              <View
+                style={[
+                  styles.historyProgressFill,
+                  { backgroundColor: calories > 3000 ? "red" : "green", width: `${(calories / 5000) * 100}%` },
+                ]}
+              />
+            </View>
+          ))}
+        </View>
+        <Footer theme={theme} navigation={navigation} />
+      </ScrollView>
+    );
+  }catch{
+
+    //returns an error if the page was not able to load.
+    console.log("Failed to load home screen page. Please try again");
+  }
+};
 // export HomeScreen as a external module for referencing
 export default HomeScreen2;
